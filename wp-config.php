84aa98d648d98d2fee0ece93e9422880815a7dd0
Joscha Holzhäuer <j.holzhaeuer@outlook.com>
2016-12-23 10:52:56 +0100

    TASK: updated config

--- wp-config.php
+++ wp-config.php
@@ -8,35 +8,35 @@ if ( file_exists( dirname( __FILE__ ) . '/wp-config-local.php' ) ) {
 } else {
 
     // Live Server Database Settings
-    define('DB_NAME',     'db_DBCLIENTNAME'); // enter database name of client
+    define('DB_NAME',     'db_b-wp-gulp'); // enter database name of client
     define('DB_USER',     'root');            // enter user name
-    define('DB_PASSWORD', '');                // enter password
+    define('DB_PASSWORD', 'root');                // enter password
     define('DB_HOST',     'localhost');
 
     // Overwrites the database to save keep edeting the DB
-    define('WP_HOME','http://yourwebsite.com');
-    define('WP_SITEURL','http://yourwebsite.com');
+    define('WP_HOME','http://b-wp-gulp');
+    define('WP_SITEURL','http://b-wp-gulp');
 
     // Turn Debug off on live server
     define('WP_DEBUG', false);
 }
 
 // Never use wp_ always use your own to prevent some hacks
-$table_prefix  = 'bwpg_';
+$table_prefix  = 'bwpg_'; //enter table prefix of client
 
-define('DB_CHARSET', 'utf8mb4');
+define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
 // Generate specific Wordpress keys
 // @link: https://api.wordpress.org/secret-key/1.1/salt/
-define('AUTH_KEY',         ')2OR<Lr#jx~CFV+BhUR4CMLNG}uG[7Z/|X0BY$#TX8Q<Ilgh?1q5UEPS<Jz)oH%x');
-define('SECURE_AUTH_KEY',  'hi^*inj;OMR`/b}m!lZuI3tdC]Z([A;gE jXy-e/6](EJm+oso^i#67-[5&1= ,d');
-define('LOGGED_IN_KEY',    'WV#I;^Bt0L,Y7f?YwTz-v7_0oYml2K>+QCf$@zmF}lfEV*&KT,*Ei}F+PZ%@@_#p');
-define('NONCE_KEY',        'hq<3CRX]5f;O:2<#L!>]v^4$8WKM:{SxlRQ}(j]}[;bF/1k8]H6I>Ujdj^q{`2d_');
-define('AUTH_SALT',        '#;YsKCoL2{5!^QzO8AT=s8q44c&h,pc.#TVCD{q-_%T2u/R$(=4_<R9{)}WKU}`0');
-define('SECURE_AUTH_SALT', 'yO3i`24h6BHAQ1||@cr.ex3bVj&|^p:z,gctEzh3v?sZ!HH[-FC5j)PL.ULwsTRZ');
-define('LOGGED_IN_SALT',   'vYb-~_OxnOM>_gLW~gsUr>Z6MU%-kJMA/Dz{`h91EV2@qi=U:g>$.Se]JIK`2nrp');
-define('NONCE_SALT',       '=k)m;{?_D`peXmsz(!$,{{HUIl@W8DgYT-JihxzcijSkw=oG!N6JuY,fj@klT!VE');
+define('AUTH_KEY',         'v#%PIbkS)cT0z|)zunjM/yh3N`F)s]Va+u*M;}Bsglp 6t-hnf4vhb,(FBdK#z5N');
+define('SECURE_AUTH_KEY',  'K|iPvT4wyU_v&P$O`8XEaE:+kL;rPEDw{6:tQ:|1JB=_+:$EJ|=-TKUdD|Foo@8S');
+define('LOGGED_IN_KEY',    ') BIb;F[P6Z`qG)P@|&tbX(>jm>j_6ZSxeX,ux39o=3&P+Dz>^3#R4i,y1>:$*Lc');
+define('NONCE_KEY',        'im.Dz1~u4|d2Mw]$1|M|*jQx=]JUzwWV OeykItQ!H>]D9(Kd[58{F~K-}Oz/E;f');
+define('AUTH_SALT',        'Vn~cx&iv;;Ki&pUU9Np,DH|aWe$NP#L+3B!6v:m/!?DX!t`n*=Jk+y2?@ 6}f&ma');
+define('SECURE_AUTH_SALT', 'F=$$>rBGzqM/{]wVid@Uxd,2HGq[.KYI=LOwxo|E9(T+|Q+,<*$<3at<~7xh|VZg');
+define('LOGGED_IN_SALT',   '10W}9J/`F1$g$:%~xD%1Q.3yjz8GG2c?O_-irm..N@ wmE:`RuJ~kDq42TV dqI}');
+define('NONCE_SALT',       'J5nuf5c%[~/4!/7,{F!l?V Zu=2+l?OVFzi!2++sMiR;N$.sC}-C2p|K~MshnD[)');
 define('WPLANG', '');
 
 if ( !defined('ABSPATH') )
